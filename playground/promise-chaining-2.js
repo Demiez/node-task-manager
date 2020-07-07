668040ad909539e8fcfbe2c6ce9810913d22f4d3
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-07-07 15:19:41 +0300

    check

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,6 +3,8 @@ const Task = require('../src/models/task');
 
 const id = '5f0432198919f72cd0a28e54'
 
+//check
+
 Task.findOneAndDelete({ _id: id}).then((task) => {
     if(!task) {
         return console.log ('Cannot find the task');
