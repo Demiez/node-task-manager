1618bb1435ba36f0c2e82bcb833eb4f2c837e20f
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-07-07 16:32:44 +0300

    refactoring to async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,18 +3,26 @@ const Task = require('../src/models/task');
 
 const id = '5f0432198919f72cd0a28e54'
 
-//check
-
-Task.findOneAndDelete({ _id: id}).then((task) => {
-    if(!task) {
-        return console.log ('Cannot find the task');
-    }
+// Task.findOneAndDelete({ _id: id}).then((task) => {
+//     if(!task) {
+//         return console.log ('Cannot find the task');
+//     }
     
-    console.log(task);
+//     console.log(task);
 
-    return Task.countDocuments({completed: false});
-}).then((result) => {
+//     return Task.countDocuments({completed: false});
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findOneAndDelete({ _id: id});
+    const count = await Task.countDocuments({completed: false});
+    return count;
+};
+
+deleteTaskAndCount(id).then((result) => {
     console.log(result);
-}).catch((e) => {
-    console.log(e);
-})
+}).catch((e) => console.log(e));
