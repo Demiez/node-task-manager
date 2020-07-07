a6703b898a5c26bb32b260d0e0b63c0df3434473
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-07-07 14:29:53 +0300

    Init commit

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/task-manager-api';
+
+module.exports = CONNECTION_STRING;
---
+++ src/db/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const CONNECTION_STRING = require('../constants/constants');
+
+mongoose.connect(CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
