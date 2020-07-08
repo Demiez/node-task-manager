a6f01aae7c5e14586621a862b440c6fcd48dd820
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-07-08 17:19:31 +0300

    finialized authentication for users and tasks

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 const { SECRET_STRING } = require('../constants/constants');
 
 const userSchema = new mongoose.Schema({
@@ -53,6 +54,12 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
 userSchema.methods.toJSON = function () {
     const user = this;
     const userObject = user.toObject();
@@ -98,6 +105,14 @@ userSchema.pre('save', async function (next) {
 
     next();
 });
+
+userSchema.pre('remove', async function (next) {
+    const user = this;
+
+    await Task.deleteMany({owner: user._id});
+
+    next();
+})
 
 const User = mongoose.model('User', userSchema);
 
