778a1a6a6ac921721d9c7f3c59009e17a18906f4
Dmytro Petrenko <dmg.petrenko@gmail.com>
2020-07-23 13:02:43 +0300

    unit tests updated structure

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,26 +1,9 @@
 const request = require('supertest');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const app = require('../src/app');
 const User = require('../src/models/user');
+const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
 
-const userOneId = new mongoose.Types.ObjectId();
-const userOne = {
-    _id: userOneId,
-    name: 'Andrew',
-    email: 'andrew@example.com',
-    password: '123pass!',
-    tokens: [
-        {
-            token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
-        },
-    ],
-};
-
-beforeEach(async () => {
-    await User.deleteMany();
-    await new User(userOne).save();
-});
+beforeEach(setupDatabase);
 
 test('Should signup a new user', async () => {
     await request(app)
@@ -62,10 +45,7 @@ test('Should get profile for user', async () => {
 });
 
 test('Should not get profile for not authenticated user', async () => {
-    await request(app)
-        .get('/users/me')
-        .send()
-        .expect(401);
+    await request(app).get('/users/me').send().expect(401);
 });
 
 test('Should delete account for authenticated user', async () => {
@@ -77,20 +57,15 @@ test('Should delete account for authenticated user', async () => {
 });
 
 test('Should not delete account for not authenticated user', async () => {
-    await request(app)
-        .delete('/users/me')
-        .send()
-        .expect(401);
+    await request(app).delete('/users/me').send().expect(401);
 });
 
 test('Database should be changed correctly', async () => {
-    const response = await request(app)
-        .post('/users')
-        .send({
-            name: 'Dmytro',
-            email: 'dmytro@example.com',
-            password: 'MyPass123!',
-        })
+    const response = await request(app).post('/users').send({
+        name: 'Dmytro',
+        email: 'dmytro@example.com',
+        password: 'MyPass123!',
+    });
     const user = await User.findById(response.body.user._id);
     expect(user).not.toBeNull();
     //expect(response.body.user.name).toBe('Dmytro');
@@ -99,18 +74,16 @@ test('Database should be changed correctly', async () => {
             name: 'Dmytro',
             email: 'dmytro@example.com',
         },
-        token: user.tokens[0].token
+        token: user.tokens[0].token,
     });
     expect(user.password).not.toBe('MyPass123!');
 });
 
 test('Database should save new token correctly', async () => {
-    const response = await request(app)
-        .post('/users/login')
-        .send({
-            email: userOne.email,
-            password: userOne.password,
-        });
+    const response = await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password,
+    });
     const user = await User.findById(userOneId);
     expect(response.body.token).toBe(user.tokens[1].token);
 });
@@ -130,7 +103,7 @@ test('Should upload image for avatar', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .attach('avatar', 'tests/fixtures/profile_avatar.jpg')
         .expect(200);
-    
+
     const user = await User.findById(userOneId);
     expect(user.avatar).toEqual(expect.any(Buffer));
 });
@@ -140,7 +113,7 @@ test('Should update valid user fields', async () => {
         .patch('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
-            name: 'Bob'
+            name: 'Bob',
         })
         .expect(200);
 
@@ -153,8 +126,7 @@ test('Should not update invalid user fields', async () => {
         .patch('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send({
-            experience: 'middle'
+            experience: 'middle',
         })
         .expect(400);
-})
-
+});
